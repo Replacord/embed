57b7c05addc35d9e0a39299ac7bc8cc717188a07
Zombiefiedgamingyt <93808177+Zombiefiedgamingyt@users.noreply.github.com>
2022-02-07 14:00:19 -0600

    Update account.js

--- account/account.js
+++ account/account.js
@@ -167,9 +167,7 @@ dbRef.get().then((snapshot) => {
   
   let data = snapshot.val()
   let bioo = data.bio
-  let biotext = bioo.replaceAll('domain', document.domain) 
-	
-  alert(biotext)
+  let biotext = bioo.replaceAll('%domain%', document.domain) 
   
   html.h1(html.img(data.pfp, '', 'profilepfp'))
   html.h1(data.username + "'s Profile", '', 'profileuser')
